4fcb890ba443770fe18ee70b50a1ba8801dc4443
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-04-23 12:56:07 +0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,28 @@
+//slice functions
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice([...arr]));
+console.log(arr.slice());
+
+//SPLICE
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr)
+arr.splice(1, 2)
+console.log(arr);
+
+
+//reverse
+arr = ['d', 'a', 'n', 'i', 'e', 'l'];
+const arr2 = ['l', 'u', 'n', 't', 'r', 'a', 's '];
+console.log(arr2.reverse(arr));
+console.log(arr2)
+
+//concat
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log(...arr, ...arr2)
