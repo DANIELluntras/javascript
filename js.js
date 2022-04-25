720f4f4551cb6e52f029547ce2139b8b9d949baa
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-04-25 14:19:26 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,20 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+    if (movement > 0) {
+        console.log(` movament ${i + 1}:  you depositori ${movement}`);
+    } else {
+        console.log(` movament ${i + 1}: you withdrew  ${Math.abs(movement)}`);
+    }
+}
+//two different properties but have the same answer
+
+
+console.log('-----for each ------')
+movements.forEach(function(mov, i, arr) {
+    if (mov > 0) {
+        console.log(` movament ${i + 1}:  you depositori ${mov}`);
+    } else {
+        console.log(` movament ${i + 1}: you withdrew  ${Math.abs(mov)}`);
+    }
+});
