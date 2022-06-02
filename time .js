4dfb651fe3d9fd42a7e7ced2dcb976f196a5d31b
luntras Daniel <luntrasdaniel12@gmail.com>
2022-06-02 16:02:58 +0300

    mathod for  Time .js

---
+++ time .js
@@ -0,0 +1,14 @@
+const future = new Date(2037,10,19, 15, 23);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+console.log(new Date(2142249780000));
+console.log(Date.now());
+future.setFullYear(2040)
+console.log(future)
