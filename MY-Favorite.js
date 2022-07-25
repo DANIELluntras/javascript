03a0a833cfede7f6551b4662a12a954c5942731f
luntras Daniel <luntrasdaniel12@gmail.com>
2022-07-25 23:54:21 +0300

    is my favorite mathod

---
+++ MY-Favorite.js
@@ -0,0 +1,7 @@
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabs.forEach(t=> t.addEventListener('click',()=>
+console.log(';=(')
+));
