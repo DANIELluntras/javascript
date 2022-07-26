caec677cfe8de32f34fd53c6fbecf6a244005648
luntras Daniel <luntrasdaniel12@gmail.com>
2022-07-26 22:52:30 +0300

    Update tabbs component.js

--- tabbs component.js
+++ tabbs component.js
@@ -7,19 +7,20 @@ const tabsContent = document.querySelectorAll('.operations__content');
 tabsContainer.addEventListener('click', function(e){
 console.log('nini');
 const clcked = e.target.closest('.operations__tab');
-const cicked = e.target.parentElement;
-console.log(cicked);
-console.log(clcked);
+
+
 // Guard clause
-if(!cicked)return;
+if(!clcked)return;
+
 //remove active classes
 tabs.forEach(t=>t.classList.remove('operations__tab--active'));
 tabsContent.forEach(c=>c.classList.remove('operations__content--active'));
 
 //Active tab 
 clcked.classList.add('operations__tab--active');
+
 //Active content area 
-console.log(clcked.dataset.tab);
+
 document.querySelector(`.operations__content--${clcked.dataset.tab}`)
 .classList.add('operations__content--active')
 });
