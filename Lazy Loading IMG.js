f3238dce898ed463d24801076eb72cc2371a2bb3
luntras Daniel <luntrasdaniel12@gmail.com>
2022-08-01 17:33:39 +0300

    Create Lazy Loading IMG.js

---
+++ Lazy Loading IMG.js
@@ -0,0 +1,31 @@
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+//acesta este pentru a blura pentru o mica perioada de vreme o imagine 
+//si pentru a sterge functia pusa  hidden - ia este pusa pentru a blura , 
+//si pusa aceasta ca sa fie stearsa ---- entry.target.classList.remove('lazy-img');
+
