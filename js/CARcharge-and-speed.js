f6e3b279a26c10e7afd38f1ab412e1bb0dfd08da
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-08-31 21:08:07 +0300

    sdaer

---
+++ js/CARcharge-and-speed.js
@@ -0,0 +1,48 @@
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+
+}
+//link the prototype 
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+    this.charge = chargeTo;
+
+}
+
+
+EV.prototype.accelerate = function () {
+    this.speed += 10;
+    this.charge--;
+    console.log(`${this.make} is going at ${this.speed}km/h  with a charge of ${this.charge}🔋`)
+}
+
+
+
+
+
+
+
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla)
+tesla.brake();
+tesla.accelerate()
