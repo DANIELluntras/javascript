fb8b17e5068a6f39e36b2108a17035d3cf42c9ac
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-09-08 22:41:52 +0300

    chore:added another class

--- js/Another Class Example.js
+++ js/Another Class Example.js
@@ -1,45 +1,58 @@
-
-
-//Another Class Example 
-
-//aicia le declaram ca exista  ca parametri 
 class Account {
+    local = navigator.language;
+
+    _movements = [];
+
+
+
+
+
+
     constructor(owner, moneda, pin) {
         this.owner = owner;
         this.moneda = moneda;
-        this.pin = pin;
-        this.movements = []
-        this.locale = navigator.language
+          this._pin = pin;
+        //procted proprety
+        // this._movements = [];
+        // this.locale = navigator.language;
 
-        console.log(`Thancks for opening an account ${owner} and i change my ${pin}`)
+        console.log(
+            `Thancks for opening an account ${owner} and i change my ${pin}`
+        );
     }
 
-    //public interface
+      //public interface
+    getMovements() {
+        return this._movements;
+    }
+
     deposit(val) {
-        this.movements.push(val)
-    }
+          this._movements.push(val);
+      }
+
     withdraw(val) {
-        this.deposit(-val)
+        this.deposit(-val);
     }
 
-    approveLoad(val) {
+    _approveLoad(val) {
         return true;
     }
 
     requestLoad(val) {
-        if (this.approveLoad(val)) {
-            this.deposit(val);
-            console.log(`load approved`);
+          if (this._approveLoad(val)) {
+              this.deposit(val);
+              console.log(`load approved`);
         }
     }
 }
-// contruim noi persoane 
-const acc1 = new Account('Daniel', 'lei', 1111)
-console.log(acc1)
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+    // contruim noi persoane
+const acc1 = new Account('Daniel', 'lei', 1111);
+console.log(acc1);
+acc1._movements.push(250);
+acc1._movements.push(-140);
+console.log(acc1.getMovements());
 acc1.deposit(350);
 acc1.withdraw(134);
-acc1.requestLoad(1)
+acc1.requestLoad(1);
 
-console.log(acc1)
+console.log(acc1);
