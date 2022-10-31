e3eba75b866a5c31388c052b39b21547b6653279
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-10-31 22:07:19 +0200

    Update script.js

--- import and export/script.js
+++ import and export/script.js
@@ -1,17 +1,46 @@
-console.log('Import modal')
-//blocking code 
-console.log('start')
-const shoppingCart = 20;
-export const cart = [];
-export const addtocart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`)
-}
-const totalPrice = 237;
-const totalQuantity = 23;
-export { totalPrice, totalQuantity }
+// Inpoting module 
+// addtocart('bread' , 'gaina');
+// console.log(price , totalQuantity)
+// import { addtocart  , totalPrice as price, totalQuantity } from "./shoppingCart.js";
+// console.log('Export module');
+// import * as ShoppingCart from'./shoppingCart.js';
 
-export default function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`)
+// ShoppingCart.addtocart('bread',5 );
+
+
+
+import add, { cart } from './shoppingCart.js';
+add('pizza ', 2)
+add('bread ', 3)
+add('dog ', 24)
+
+console.log(cart);
+
+
+// console.log('Start fetching')
+// const res = await  fetch('https://jsonplaceholder.typicode.com/posts')
+
+// const data  = await res.json()
+// console.log(data)
+// console.log('something')
+
+const getLastPost = async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+    const data = await res.json()
+    console.log(data)
+    return { title: data.at(-1).title, text: data.at(-1).body }
 }
+const lastPost = getLastPost();
+console.log(lastPost);
+//not very clean
+lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2)
+
+
+
+
+
+
