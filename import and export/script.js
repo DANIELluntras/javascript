156fa041cec2e2648fd8ec5924f88010b3872589
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-10-31 22:26:32 +0200

    Update script.js

--- import and export/script.js
+++ import and export/script.js
@@ -1,46 +1,57 @@
-// Inpoting module 
-// addtocart('bread' , 'gaina');
-// console.log(price , totalQuantity)
-// import { addtocart  , totalPrice as price, totalQuantity } from "./shoppingCart.js";
-// console.log('Export module');
-// import * as ShoppingCart from'./shoppingCart.js';
+// console.log(cart);
 
-// ShoppingCart.addtocart('bread',5 );
+// // console.log('Start fetching')
+// // const res = await  fetch('https://jsonplaceholder.typicode.com/posts')
 
+// // const data  = await res.json()
+// // console.log(data)
+// // console.log('something')
 
-
-import add, { cart } from './shoppingCart.js';
-add('pizza ', 2)
-add('bread ', 3)
-add('dog ', 24)
-
-console.log(cart);
-
-
-// console.log('Start fetching')
-// const res = await  fetch('https://jsonplaceholder.typicode.com/posts')
+// const getLastPost = async function(){
+//     const res = await  fetch('https://jsonplaceholder.typicode.com/posts')
 
 // const data  = await res.json()
 // console.log(data)
-// console.log('something')
+// return{title:data.at(-1).title,text:data.at(-1).body}
+// }
+// const lastPost = getLastPost();
+// console.log(lastPost);
+// //not very clean
+// lastPost.then(last => console.log(last));
 
-const getLastPost = async function () {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+// const  lastPost2 = await getLastPost();
+// console.log(lastPost2)
 
-    const data = await res.json()
-    console.log(data)
-    return { title: data.at(-1).title, text: data.at(-1).body }
-}
-const lastPost = getLastPost();
-console.log(lastPost);
-//not very clean
-lastPost.then(last => console.log(last));
+const ShoppingCart2 = (function () {
+    const cart = [];
+    const shipingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+    const addtocart = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} added to cart - siping cost is ${shipingCost}`);
+    };
+    const orderStock = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} order from Supplimer`);
+    };
+    return {
+        addtocart,
+        totalPrice,
+        cart,
+        totalQuantity
+    }
+})();
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2)
 
 
+ShoppingCart2.addtocart('apple', 4)
 
+ShoppingCart2.addtocart('Mere', 45)
+ShoppingCart2.addtocart('Gaini', 2)
+ShoppingCart2.addtocart('iepuri', 3)
+ShoppingCart2.addtocart('bitcoin', 34)
+console.log(ShoppingCart2)
 
 
 
