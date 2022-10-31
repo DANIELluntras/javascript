d47368e844fcee484508f27d1e029dbe377f608e
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-10-31 22:27:19 +0200

    Update shoppingCart.js

--- import and export/shoppingCart.js
+++ import and export/shoppingCart.js
@@ -1,17 +1,17 @@
-console.log('Import modal')
-//blocking code 
-console.log('start')
+console.log('Import modal');
+//blocking code
+console.log('start');
 const shoppingCart = 20;
 export const cart = [];
 export const addtocart = function (product, quantity) {
     cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`)
-}
+    console.log(`${quantity} ${product} added to cart`);
+};
 const totalPrice = 237;
 const totalQuantity = 23;
-export { totalPrice, totalQuantity }
+export { totalPrice, totalQuantity };
 
 export default function (product, quantity) {
     cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`)
+    console.log(`${quantity} ${product} added to cart`);
 }
