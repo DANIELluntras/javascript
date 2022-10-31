3a19be027a1217de73c24e8b4c518c7f6530fd21
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-10-31 22:53:11 +0200

    Update script.js

--- import and export/script.js
+++ import and export/script.js
@@ -1,3 +1,19 @@
+// // Inpoting module
+// // addtocart('bread' , 'gaina');
+// // console.log(price , totalQuantity)
+// // import { addtocart  , totalPrice as price, totalQuantity } from "./shoppingCart.js";
+// // console.log('Export module');
+// // import * as ShoppingCart from'./shoppingCart.js';
+
+import shoppingCart from "./shoppingCart";
+
+// // ShoppingCart.addtocart('bread',5 );
+
+// import add, {cart} from './shoppingCart.js';
+// add('pizza ',2)
+// add('bread ',3)
+// add('dog ',24)
+
 // console.log(cart);
 
 // // console.log('Start fetching')
@@ -22,36 +38,50 @@
 // const  lastPost2 = await getLastPost();
 // console.log(lastPost2)
 
-const ShoppingCart2 = (function () {
-    const cart = [];
-    const shipingCost = 10;
-    const totalPrice = 237;
-    const totalQuantity = 23;
-    const addtocart = function (product, quantity) {
-        cart.push({ product, quantity });
-        console.log(`${quantity} ${product} added to cart - siping cost is ${shipingCost}`);
-    };
-    const orderStock = function (product, quantity) {
-        cart.push({ product, quantity });
-        console.log(`${quantity} ${product} order from Supplimer`);
-    };
-    return {
-        addtocart,
-        totalPrice,
-        cart,
-        totalQuantity
-    }
-})();
+// const ShoppingCart2 = (function () {
+//     const cart = [];
+//     const shipingCost = 10;
+//     const totalPrice = 237;
+//     const totalQuantity = 23;
+//     const addtocart = function (product, quantity) {
+//         cart.push({ product, quantity });
+//         console.log(`${quantity} ${product} added to cart - siping cost is ${shipingCost}`);
+//     };
+//     const orderStock = function (product, quantity) {
+//         cart.push({ product, quantity });
+//         console.log(`${quantity} ${product} order from Supplimer`);
+//     };
+//     return {
+//         addtocart,
+//         totalPrice,
+//         cart,
+//         totalQuantity
+//     }
+// })();
 
 
 
-ShoppingCart2.addtocart('apple', 4)
+// ShoppingCart2.addtocart('apple',4)
 
-ShoppingCart2.addtocart('Mere', 45)
-ShoppingCart2.addtocart('Gaini', 2)
-ShoppingCart2.addtocart('iepuri', 3)
-ShoppingCart2.addtocart('bitcoin', 34)
-console.log(ShoppingCart2)
+// ShoppingCart2.addtocart('Mere',45)
+// ShoppingCart2.addtocart('Gaini',2)
+// ShoppingCart2.addtocart('iepuri',3)
+// ShoppingCart2.addtocart('bitcoin',34)
+// console.log(ShoppingCart2)
+
+//CommonJS Modules - 275
+// //export
+// export.addtocart  = function (product, quantity) {
+//         cart.push({ product, quantity });
+//            console.log(`${quantity} ${product} order from Supplimer (shipping cost is ${shoppingCart}`);
+           
+//         };
+
+// //import
+// const {addtocart} = require('./shoppingCart.js');
+
+//- 276 - A Brief Introduction to the Command Line
+
 
 
 
