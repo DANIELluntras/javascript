73771452757893e0a6ae14622de199f6be0dad87
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2023-01-04 16:11:48 +0200

    chore:added new server node.js

---
+++ node.js/new server😍.js
@@ -0,0 +1,33 @@
+
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json `, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+    if (pathName === "/" || pathName === "/overview") {
+        res.end("This is the OVERVIEW");
+    } else if (pathName === "/product") {
+        res.end("this is the product");
+    } else if (pathName === "/api") {
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-own-header": "hello-world",
+        });
+    }
+
+    res.end(`<h1> Page not found!</h1>`);
+});
+
+server.listen(3000, "127.0.0.1", () => {
+    console.log("listening to request on port 3000");
+});
+// node index.js
